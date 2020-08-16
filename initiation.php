c4740ca35eaedf615a135e1b4224df451a9ce3e8
Vincent Herail <vincent.herail@gmail.com>
2020-08-16 16:40:18 +0200

    project sent

---
+++ initiation.php
@@ -0,0 +1,102 @@
+
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <title>Maison Maître</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Créations digitales sur-mesures. Site Web, réseaux sociaux, applis mobiles, graphisme...">
+    <meta name="keywords"  content="agence,web,developpement,montpellier,marketing,communication,design,reseaux,sociaux" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-158746714-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-158746714-1');
+    </script>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="shortcut icon" href="img/chapeau.png">
+  </head>
+
+  <body id="haut">
+
+        <!--*********************** NAVBAR ******************-->   
+<?php require "navbar.html"?>
+
+
+        <!--*********************** HEADER ****************** -->
+
+        <!--*********************** Presentation ****************** -->
+        <div class="d-flex flex-row flex-wrap align-content-center justify-content-around container-fluid">
+            <div class="col-lg-10 block my-5 text-center">
+                <img src="img/vangogh.png" class="pb-5 mx-auto d-block" alt="..."><span class="text-success">Compte Instagram @lesecretdevangogh</span><br/><br/>
+                Commençant sa formation à l'âge de 28 ans, Van Gogh ne peindra que durant 10 années, période éclair au cours de laquelle il réalise plus de 2000 œuvres.
+                Qu'est-ce qui peut expliquer cette trajectoire? Comment ces tableaux à la technique "rudimentaire" peuvent-ils aujourd'hui encore toucher le coeur de millions de personnes à travers le monde?<br/> <br/>
+                La réponse est simple : Van Gogh avait compris la nature profonde de l'art, tant d'un point de vue philosophique, qu'esthétique. Ce savoir, loin d'être magique, est non-seulement accessible au commun des mortels, mais nous pensons qu'il est nécessaire à la formation intellectuelle de toute personne s'inscrivant dans une démarche de communication. </div>
+            <div class="col-10 col-lg-3 m-2">
+                <img src="img/couverture2.jpg" class="py-4 mx-auto d-block" alt="...">
+                <h5 class="mt-0 text-warning pt-3 ">Principe 1 - Du sublime et du beau</h5>
+                <p class="block text-justify"> <span class="text-success">Mars 2020</span> <br/><i>Les goûts et les couleurs ne se discutent pas?</i> C'est faux. Il existe des lois qui régissent les rapports entre formes et couleurs. Il existe également une hiérarchie des sujets que traite l'art, et des ressentis qu'il provoque. 
+                </p>
+            </div>
+            <div class="col-10 col-lg-3 m-2">
+                <img src="img/couverture3.jpg" class="py-4  mx-auto d-block" alt="...">
+                <h5 class="mt-0 text-warning pt-3">Principe 2 - De la nature et de l'art                </h5>
+                <p class="block text-justify">
+                    <span class="text-success">Mars 2020 </span><br/>
+                    Quand la nature est belle, le peintre sait qu’elle est belle, les promeneurs aussi, mais la nature n’en sait rien. Ainsi la beauté n’existe qu’à la condition de recevoir une seconde vie dans la pensée humaine. 
+
+                </p> <br/>
+            </div>
+            <div class="col-10 col-lg-3 m-2">
+                <img src="img/couverture4.jpg" class="py-4 mx-auto d-block" alt="...">
+                <h5 class="mt-0 text-warning pt-3">Principe 3 - Grandeur et mission de l'art
+                </h5>
+                <p class="block text-justify">
+                    <span class="text-success">Avril 2020</span><br/> L'art a bel et bien une mission, il n'est pas gratuit. Il peut être le fruit d'une commande, mais il ne saurait être commandé. L'art est libre, vrai, ou il n'est pas. Il est ce témoignage qui nous rend immortel.
+
+                </p>
+            </div>
+            <div class="col-10 col-lg-3 m-2">
+                <img src="img/couverture5.jpg" class="py-4 mx-auto d-block " alt="...">
+                <h5 class="mt-0 text-warning pt-3">Principe 4 - De l'imitation et du style
+                </h5>
+                <p class="block text-justify">
+                    <span class="text-success">Avril 2020</span><br/> 
+                    L'artiste imite la nature non pas telle qu'elle est, mais tel qu'il est lui. De là naissent les divers caractères de l’art, que l’on nomme "styles". Quant à la perfection, elle est atteinte quand la personnalité s'efface.
+
+                </p> <br/>
+            </div>
+            <div class="col-10 col-lg-3 m-2">
+                <img src="img/couverture6.jpg" class="py-4 mx-auto d-block " alt="...">
+                <h5 class="mt-0 text-warning pt-3">Principe 5 - Du dessin et de la couleur
+                </h5>
+                <p class="block text-justify">                    
+                    <span class="text-success">Mai 2020</span><br/> Le dessin, anciennement écrit "dessein", est un projet de l'esprit, il est le sexe masculin de l’art. La couleur en est le sexe féminin, elle est le charme, l'expression et la grâce.
+
+                </p>
+            </div>
+            <div class="col-10 col-lg-3 m-2">
+                <img src="img/couverture7.jpg" class="py-4 mx-auto d-block" alt="...">
+                <h5 class="mt-0 text-warning pt-3 ">Principe 6 - De la figure humaine
+                </h5>
+                <p class="block text-justify">
+                <span class="text-success">Mai 2020</span><br/> 
+                L'objet le plus digne des études en qui sont fondus tous les contrastes et toutes les harmonies, la variété et l’unité, le pair et l’impair, la liberté et la règle, la disparité et la symétrie.
+                </p>
+            </div>
+        </div>
+
+    <!-- Optional JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body> 
+</html>
